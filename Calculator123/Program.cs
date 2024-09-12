b85a50b7aade4cd92b37b61a7aece8f82f2fd90b
1 <1@HUAWEI>
2024-09-12 11:12:08 +0300

    Добавьте файлы проекта.

---
+++ Calculator123/Program.cs
@@ -0,0 +1,43 @@
+﻿//// See https://aka.ms/new-console-template for more information
+//Console.WriteLine("Hello, World!");
+
+start:
+double firstvalue, secoundvalue;
+string operatorV;
+
+Console.WriteLine("Введите первое значение: ");
+firstvalue = Double.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите второе значение: ");
+secoundvalue = Double.Parse(Console.ReadLine());
+
+Console.WriteLine("Выберите медод операции: + | - | * | / |");
+operatorV = Console.ReadLine();
+
+switch (operatorV)
+{
+    case "+":
+        Console.WriteLine(firstvalue + secoundvalue);
+        goto start;
+
+    case "-":
+        Console.WriteLine(firstvalue - secoundvalue);
+        goto start;
+
+    case "*":
+        Console.WriteLine(firstvalue * secoundvalue);
+        goto start;
+
+    case "/":
+        if (firstvalue == 0)
+            Console.WriteLine(0);
+        else if (secoundvalue == 0)
+            Console.WriteLine(0);
+        else
+            Console.WriteLine(firstvalue / secoundvalue);
+        goto start;
+
+    default:
+        Console.WriteLine("Неизвестная команда, повторите попытку!");
+        goto start;
+}
